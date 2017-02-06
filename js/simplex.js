aa18eae711738fd788c3228e4f9248987e835d6f
Esteban <stevedebuchy@gmail.com>
2017-02-06 20:55:16 +0100

    ok avec config chargée

--- js/simplex.js
+++ js/simplex.js
@@ -4,6 +4,23 @@ console.log("open simplex");
 
 var form = document.querySelector("form");
 
+// boutons
+var boutonVariables = document.getElementById("variables");
+boutonVariables.addEventListener("click", clic);
+var boutonContraintes = document.getElementById("contraintes");
+boutonContraintes.addEventListener("click", clic);
+var boutonCalculer = document.getElementById("calculer");
+boutonCalculer.addEventListener("click", calcul);
+
+// variables globales 
+var T = true;   // traces debug booléen
+var contraintes;
+var variables;
+var somme;
+var lignepivot = new Array();
+var reponse = new Array();
+
+
 // objects prototype
 var maxZ = {
         x1 :0, x2 :0, x3 :0, x4 :0, x5 :0,
@@ -52,15 +69,20 @@ var c1 = {
 var c2 = Object.create(c1);
 
 // execution
-function calcul(){
+function init(){
 
 	// affichage énoncé et résultat
 	var text = '<p><label>'+maxZ.afficher()+'</p></label>';
 	document.getElementById("reponse").innerHTML = text;
 
+	//initialise les tableaux 
+	for(var i=0; i<contraintes.value; i++){
+		lignepivot[i] = false;
+		reponse[i] = 0;
+	}
+
 
 	// ***************creation matrice*****************************
-
 	var matrice = new Array(); 
 	for(var i=0; i<Number(contraintes.value)+1;i++)
 		matrice[i] = new Array();
@@ -86,6 +108,7 @@ function calcul(){
 	matrice[1][1] = form.elements.x2c2.value; // c2x2
 	matrice[1][somme] = form.elements.x6c2.value; // maxcontraintec2
 
+
 	// variables d'écart
 	for (var i=0; i<contraintes.value; i++)
 		matrice[i][Number(variables.value)+i] = 1;
@@ -98,7 +121,6 @@ function calcul(){
 		matrice[2][0] = form.elements.x1c3.value; // c3x1
 		matrice[2][1] = form.elements.x2c3.value; // c3x2
 		matrice[2][somme] = form.elements.x6c3.value; // maxcontraintec3
-	}
 	// 4 contraintes	
 	if(contraintes.value > 3){ 
 		matrice[3][0] = form.elements.x1c4.value; // c4x1
@@ -112,15 +134,16 @@ function calcul(){
 		matrice[4][somme] = form.elements.x6c5.value; // maxcontraintec5
 	}
 
-	// ************************3 variables*****************************************
+// ************************3 variables*****************************************
 
 	if(variables.value > 2){  
 		if(T) console.log("3 variables"); // debug
 		matrice[contraintes.value][2] = form.elements.x3max.value; // maxZ.x3
 		matrice[0][2] = form.elements.x3c1.value; // c1x3
 		matrice[1][2] = form.elements.x3c2.value; // c2x3
-		if(contraintes.value > 2) 
+		if(contraintes.value > 2){ 
 			matrice[2][2] = form.elements.x3c3.value; // c3x3
+		}
 		if(contraintes.value > 3) 
 			matrice[3][2] = form.elements.x3c4.value; // c4x3
 		if(contraintes.value > 4) 
@@ -158,26 +181,74 @@ function calcul(){
 	}
 	
 	// *****************************determiner le pivot*************
-
-	matrice = pivot(matrice);
-//	if(T) console.log("calcul : "+ matrice[0][0]); // debug
+	
+//	if(T) console.log("init : "+ matrice[0][0]); // debug
 
 	
 	var text = '<p><label>'+maxZ.afficher()+'</p></label>';
 	document.getElementById("reponse").innerHTML = text;
-	
+	// pour test uniquement**********************************************
+	test = false ;
+	if(test){
+	contraintes = document.getElementById("contraintes");
+	variables = document.getElementById("variables");
+	matrice[contraintes.value][0] = 4; 
+	matrice[contraintes.value][1] = 12; 
+	matrice[contraintes.value][2] = 3;
+	matrice[0][0] = 1;
+	matrice[0][1] = 0;
+	matrice[0][2] = 0; 
+	matrice[0][somme] = 1000;
+	matrice[1][0] = 0;
+	matrice[1][1] = 1;
+	matrice[1][2] = 0; 
+	matrice[1][somme] = 500;
+	matrice[2][0] = 0;
+	matrice[2][1] = 0;
+	matrice[2][2] = 1;
+	matrice[2][somme] = 1500; 
+	matrice[3][0] = 3;
+	matrice[3][1] = 6; 
+	matrice[3][2] = 2; 
+	matrice[3][somme] = 6750; 
+	}
+	}
+	// *****************************************************************
+	if(T){
+//		console.table(matrice); // debug
+	} 
+	return matrice;
 	
 //	if(T) console.table(matrice); // debug
 
 	// var text = '<p><label>x1 = </label><label id="x1"></label></br><label>x2 = </label><label id="x2"></label></br><label>x3 = </label><label id="x3"></label></br><label>x4 = </label><label id="x4"></label></br><label>x5 = </label><label id="x5"></label></br></br><label>Ooptimum est de : </label><label id="optimum"></label></p>';	
 	// algo
-	
 };
+
+function calcul(){
+	
+	var matrice = init();	
+	var encore = true;
+	while(encore){
+		matrice = pivot(matrice);
+		if(T){
+			console.table(matrice); // debug
+		}
+	encore = false;
+	for(var i=0; i<somme; i++)
+		if(matrice[contraintes.value][i] > 0){
+			encore = true;
+		}
+		 
+	}
+	for(var i=0; i<contraintes.value; i++)
+		console.log("X"+reponse[i]+" : "+matrice[i][somme]);
+}
 
 function pivot(matrice){
 	
 //	if(T) console.log("pivot : "+ matrice[0][0]); // debug
-	// trouver le pivot
+	// trouver le pivo
 	var max = matrice[contraintes.value][0];
 	var imax = 0;
 	for(var i=1; i<somme; i++)
@@ -185,28 +256,50 @@ function pivot(matrice){
 			max = matrice[contraintes.value][i];
 			imax = i;
 		}
-	var min = matrice[0][somme];
-	var imin = 0;
-	for(var i=1; i<contraintes.value; i++)
-		if(matrice[i][somme] < min){
+	var min;
+	var imin;
+	var i=0;
+	while(lignepivot[i]){
+		i++;
+//		console.log(i);
+	}
+	min = matrice[i][somme];
+	imin = i;
+	for(var i=0; i<contraintes.value; i++)
+		if(matrice[i][somme] < min && lignepivot[i] == false){
 			min = matrice[i][somme];
 			imin = i;
 		}
-	var pivot = matrice[imin][imax];		
-	if(T) console.log(" max : "+ max + " min : "+ min + " pivot : "+ pivot); // debug
+	var pivot = matrice[imin][imax];
+	lignepivot[imin] = true;
+	var rep = Number(imax)+1;
+	reponse[imin]= rep;		
+	for(var i=0; i<contraintes.value; i++){
+		console.log(i+" "+lignepivot[i]);
+	}
+	if(T) console.log(" imin : "+ imin + " imax : "+ imax + " pivot : "+ pivot); // debug
 	for(var i=0; i<=somme; i++)
 		matrice[imin][i] = matrice[imin][i]/pivot;
-	if(T){
-		console.table(matrice); // debug
-	} 
+	console.log("variable x"+rep+" vaut :"+matrice[imin][somme]);
+		
+	for(var i=0; i<= contraintes.value; i++){
+		if(matrice[i][imax]!=0 && i!=imin){
+			var multi = matrice[i][imax];
+			for(var j=0; j<=somme; j++){
+//			console.log(i+" "+j+" "+matrice[i][j]);
+//			console.log(i+" "+j+" "+matrice[imin][j]);
+//			console.log(i+" "+j+" "+multi*matrice[imin][j]);
+				matrice[i][j]=matrice[i][j]-multi*matrice[imin][j];
+			console.log(i+" "+j+" "+matrice[i][j]);
+			}
+		}		
+	}
+	for(var i=0; i<somme;i++)	
+//		console.log(i+" "+matrice[contraintes.value][i]);
 	return matrice;
-	 }
-	 // saisies variables préléminaires
+	}
 
-	 var T = true;   // traces debug booléen
-	 var contraintes;
-	 var variables;
-	 var somme;
+//*******************************au click****************************************
 	 function clic(){ 
 	 contraintes = document.getElementById("contraintes");
 	 variables = document.getElementById("variables");
@@ -225,88 +318,81 @@ function pivot(matrice){
 	}
 	text += '<th></th><th></th></tr>';
 	// ligne 2		
-	text += '<tr><th>max z = </th><td><input type="number" min=0 max=100 value=0 name="x1max" onclick="calcul();" /></td><td><input type="number" min=0 max=100 value=0 name="x2max" onclick="calcul();" /></td>';
+	text += '<tr><th>max z = </th><td><input type="number" min=0 max=100 value=0 name="x1max" onclick="init();" /></td><td><input type="number" min=0 max=100 value=0 name="x2max" onclick="init();" /></td>';
 
 	if(variables.value > 2){        
-        	text += '<td><input type="number" min=0 max=100 value=0 name="x3max" onclick="calcul();" /></td>';
+        	text += '<td><input type="number" min=0 max=100 value=0 name="x3max" onclick="init();" /></td>';
        		if(variables.value > 3){
-                	text += '<td><input type="number" min=0 max=100 value=0 name="x4max" onclick="calcul();" /></td>';
+                	text += '<td><input type="number" min=0 max=100 value=0 name="x4max" onclick="init();" /></td>';
                         if(variables.value > 4) 
-                		text += '<td><input type="number" min=0 max=100 value=0 name="x5max" onclick="calcul();" /></td>';
+                		text += '<td><input type="number" min=0 max=100 value=0 name="x5max" onclick="init();" /></td>';
                 }
         }
 	text += '<td></td><td></td></tr>';
 	// ligne 3
-	text += '<tr><th>Contrainte 1</th><td><input type="number" min=0 max=100 value=0 name="x1c1" onclick="calcul();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c1" onclick="calcul();" /></td>';
+	text += '<tr><th>Contrainte 1</th><td><input type="number" min=0 max=100 value=0 name="x1c1" onclick="init();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c1" onclick="init();" /></td>';
 	if(variables.value > 2){        
-                text += '<td><input type="number" min=0 max=100 value=0 name="x3c1" onclick="calcul();" /></td>';
+                text += '<td><input type="number" min=0 max=100 value=0 name="x3c1" onclick="init();" /></td>';
                 if(variables.value > 3){
-                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c1" onclick="calcul();" /></td>';
+                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c1" onclick="init();" /></td>';
                         if(variables.value > 4) 
-                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c1" onclick="calcul();" /></td>';
+                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c1" onclick="init();" /></td>';
                 }
         }
-        text += '<td>&lt=</td><td><input type="number" min=0 max=1000 value=0 name="x6c1" onclick="calcul();" /td></tr>';
+        text += '<td>&lt=</td><td><input type="number" min=0 max=10000 value=0 name="x6c1" onclick="init();" /td></tr>';
         // ligne 4  
-	text += '<tr><th>Contrainte 2</th><td><input type="number" min=0 max=100 value=0 name="x1c2" onclick="calcul();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c2" onclick="calcul();" /></td>';
+	text += '<tr><th>Contrainte 2</th><td><input type="number" min=0 max=100 value=0 name="x1c2" onclick="init();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c2" onclick="init();" /></td>';
 	if(variables.value > 2){        
-                text += '<td><input type="number" min=0 max=100 value=0 name="x3c2" onclick="calcul();" /></td>';
+                text += '<td><input type="number" min=0 max=100 value=0 name="x3c2" onclick="init();" /></td>';
                 if(variables.value > 3){
-                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c2" onclick="calcul();" /></td>';
+                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c2" onclick="init();" /></td>';
                         if(variables.value > 4) 
-                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c2" onclick="calcul();" /></td>';
+                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c2" onclick="init();" /></td>';
                 }
         }
-        text += '<td>&lt=</td><td><input type="number" min=0 max=1000 value=0 name="x6c2" onclick="calcul();"/td></tr>';
+        text += '<td>&lt=</td><td><input type="number" min=0 max=10000 value=0 name="x6c2" onclick="init();"/td></tr>';
         // ligne 5 
 	if(contraintes.value > 2){
-		text += '<tr><th>Contrainte 3</th><td><input type="number" min=0 max=100 value=0 name="x1c3" onclick="calcul();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c3" onclick="calcul();" /></td>';
+		text += '<tr><th>Contrainte 3</th><td><input type="number" min=0 max=100 value=0 name="x1c3" onclick="init();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c3" onclick="init();" /></td>';
         if(variables.value > 2){ 
-                text += '<td><input type="number" min=0 max=100 value=0 name="x3c3" onclick="calcul();" /></td>';
+                text += '<td><input type="number" min=0 max=100 value=0 name="x3c3" onclick="init();" /></td>';
                 if(variables.value > 3){
-                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c3" onclick="calcul();"/></td>';
+                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c3" onclick="init();"/></td>';
                         if(variables.value > 4)
-                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c3" onclick="calcul();" /></td>';
+                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c3" onclick="init();" /></td>';
                 }
         }
-        text += '<td>&lt=</td><td><input type="number" min=0 max=1000 value=0 name="x6c3" onclick="calcul();"/td></tr>';
+        text += '<td>&lt=</td><td><input type="number" min=0 max=10000 value=0 name="x6c3" onclick="init();"/td></tr>';
 	}
         // ligne 6
 	if(contraintes.value > 3){
-                text += '<tr><th>Contrainte 4</th><td><input type="number" min=0 max=100 value=0 name="x1c4" onclick="calcul();"/></td><td><input type="number" min=0 max=100 value=0 name="x2c4" onclick="calcul();"/></td>';
+                text += '<tr><th>Contrainte 4</th><td><input type="number" min=0 max=100 value=0 name="x1c4" onclick="init();"/></td><td><input type="number" min=0 max=100 value=0 name="x2c4" onclick="init();"/></td>';
         if(variables.value > 2){
-                text += '<td><input type="number" min=0 max=100 value=0 name="x3c4" onclick="calcul();" /></td>';
+                text += '<td><input type="number" min=0 max=100 value=0 name="x3c4" onclick="init();" /></td>';
                 if(variables.value > 3){
-                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c4" onclick="calcul();" /></td>';
+                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c4" onclick="init();" /></td>';
                         if(variables.value > 4)
-                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c4" onclick="calcul();" /></td>';
+                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c4" onclick="init();" /></td>';
                 }
         }
-        text += '<td>&lt=</td><td><input type="number" min=0 max=1000 value=0 name="x6c4" onclick="calcul();"/td></tr>';
+        text += '<td>&lt=</td><td><input type="number" min=0 max=10000 value=0 name="x6c4" onclick="init();"/td></tr>';
         }
         // ligne 7 
 	if(contraintes.value > 4){ 
-                text += '<tr><th>Contrainte 5</th><td><input type="number" min=0 max=100 value=0 name="x1c5" onclick="calcul();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c5" onclick="calcul();" /></td>';
+                text += '<tr><th>Contrainte 5</th><td><input type="number" min=0 max=100 value=0 name="x1c5" onclick="init();" /></td><td><input type="number" min=0 max=100 value=0 name="x2c5" onclick="init();" /></td>';
         if(variables.value > 2){
-                text += '<td><input type="number" min=0 max=100 value=0 name="x3c5" onclick="calcul();" /></td>';
+                text += '<td><input type="number" min=0 max=100 value=0 name="x3c5" onclick="init();" /></td>';
                 if(variables.value > 3){
-                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c5" onclick="calcul();" /></td>';
+                        text += '<td><input type="number" min=0 max=100 value=0 name="x4c5" onclick="init();" /></td>';
                         if(variables.value > 4)
-                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c5" onclick="calcul();" /></td>';
+                                text += '<td><input type="number" min=0 max=100 value=0 name="x5c5" onclick="init();" /></td>';
                 }
         }
-        text += '<td>&lt=</td><td><input type="number" min=0 max=1000 value=0 name="x6c5" onclick="calcul();" /td></tr>';
+        text += '<td>&lt=</td><td><input type="number" min=0 max=10000 value=0 name="x6c5" onclick="init();" /td></tr>';
         }
 	text += '</table></br>';
 	document.getElementById("injecter").innerHTML = text;
 	var text = '<p><label>'+maxZ.afficher()+'</p></label>';
 	document.getElementById("reponse").innerHTML = text;
 }
-// boutons
-var boutonVariables = document.getElementById("variables");
-boutonVariables.addEventListener("click", clic);
-var boutonContraintes = document.getElementById("contraintes");
-boutonContraintes.addEventListener("click", clic);
-var boutonCalculer = document.getElementById("calculer");
-boutonCalculer.addEventListener("click", calcul);
 
