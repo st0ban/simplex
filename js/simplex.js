ef6239ffcfb1f191c82694f42ec875b57084e1f8
Esteban <stevedebuchy@gmail.com>
2017-02-09 22:55:54 +0100

    ça marche git add .!

--- js/simplex.js
+++ js/simplex.js
@@ -10,7 +10,6 @@ boutonVariables.addEventListener("click", clic);
 var boutonContraintes = document.getElementById("contraintes");
 boutonContraintes.addEventListener("click", clic);
 var boutonCalculer = document.getElementById("calculer");
-boutonCalculer.addEventListener("click", calcul);
 
 // variables globales 
 var T = true;   // traces debug booléen
@@ -19,295 +18,21 @@ var variables;
 var somme;
 var lignepivot = new Array();
 var reponse = new Array();
-
-
-// objects prototype
-var maxZ = {
-        x1 :0, x2 :0, x3 :0, x4 :0, x5 :0,
-	afficher: function (){
-		var valeurs = "maxZ = ";
-		var plus = false; // gestion de l'arrivée du signe +
-		if(this.x1 > 0){
-                        valeurs += ""+this.x1+" X1";plus = true;
-		}
-		if(this.x2 > 0){
-			if(plus)	
-				valeurs += " + ";
-			else
-				plus = true;
-                        valeurs += ""+this.x2+" X2";
-		}
-		if(this.x3 > 0){
-                        if(plus)
-                                valeurs += " + ";
-			else
-				plus = true;
-			valeurs += ""+this.x3+" X3";
-		}
-		if(this.x4 > 0){
-                        if(plus)
-                                valeurs += " + ";
-			else
-				plus = true;
-                        valeurs += ""+this.x4+" X4";
-		}
-		if(this.x5 > 0){
-                        if(plus)
-                                valeurs += " + ";
-		        valeurs += ""+this.x5+" X5";
-		}
-		return valeurs;
-	}
-};
-var c1 = {
-        x1 :0, x2 :0, x3 :0, x4 :0, x5 :0, x6 :0, ecart :0,
-	afficher: function(){
-	return this.x1+" X1 + "+this.x2+" X2 + "+this.x3+" X3 + "+this.x4+" X4 + "+this.x5+" X5 <= "+this.x6;
-	}
-};
-
-var c2 = Object.create(c1);
-
-// execution
-function init(){
-
-	// affichage énoncé et résultat
-	var text = '<p><label>'+maxZ.afficher()+'</p></label>';
-	document.getElementById("reponse").innerHTML = text;
-
-	//initialise les tableaux 
-	for(var i=0; i<contraintes.value; i++){
-		lignepivot[i] = false;
-		reponse[i] = 0;
-	}
-
-
-	// ***************creation matrice*****************************
-	var matrice = new Array(); 
-	for(var i=0; i<Number(contraintes.value)+1;i++)
-		matrice[i] = new Array();
-
-	// ***********init*********************************************
-
-	for(var i=0; i<Number(contraintes.value)+1;i++)
-		for(var j=0; j<=somme; j++)
-			matrice[i][j] = 0;	
-
-	// **********remplissage de la matrice**************************
-
-	// fonction max
-	matrice[contraintes.value][0] = form.elements.x1max.value; // maxZ.x1
-	matrice[contraintes.value][1] = form.elements.x2max.value; // maxZ.x2
-	// maxZ.x2 = form.elements.x2max.value;
-
-	// 2 contraintes et 2 variables  au minimum
-	matrice[0][0] = form.elements.x1c1.value; // c1x1
-	matrice[0][1] = form.elements.x2c1.value; // c1x2
-	matrice[0][somme] = form.elements.x6c1.value; // maxcontraintec1
-	matrice[1][0] = form.elements.x1c2.value; // c2x1
-	matrice[1][1] = form.elements.x2c2.value; // c2x2
-	matrice[1][somme] = form.elements.x6c2.value; // maxcontraintec2
-
-
-	// variables d'écart
-	for (var i=0; i<contraintes.value; i++)
-		matrice[i][Number(variables.value)+i] = 1;
-
-	// *****************2 variables******************************************* 
-	if(T) console.log("2 variables"); // debug
-
-	// 3 contraintes
-	if(contraintes.value > 2){
-		matrice[2][0] = form.elements.x1c3.value; // c3x1
-		matrice[2][1] = form.elements.x2c3.value; // c3x2
-		matrice[2][somme] = form.elements.x6c3.value; // maxcontraintec3
-	// 4 contraintes	
-	if(contraintes.value > 3){ 
-		matrice[3][0] = form.elements.x1c4.value; // c4x1
-		matrice[3][1] = form.elements.x2c4.value; // c4x2
-		matrice[3][somme] = form.elements.x6c4.value; // maxcontraintec4
-	}
-	// 5 contraintes	
-	if(contraintes.value > 4){ 
-		matrice[4][0] = form.elements.x1c5.value; // c5x1
-		matrice[4][1] = form.elements.x2c5.value; // c5x2
-		matrice[4][somme] = form.elements.x6c5.value; // maxcontraintec5
-	}
-
-// ************************3 variables*****************************************
-
-	if(variables.value > 2){  
-		if(T) console.log("3 variables"); // debug
-		matrice[contraintes.value][2] = form.elements.x3max.value; // maxZ.x3
-		matrice[0][2] = form.elements.x3c1.value; // c1x3
-		matrice[1][2] = form.elements.x3c2.value; // c2x3
-		if(contraintes.value > 2){ 
-			matrice[2][2] = form.elements.x3c3.value; // c3x3
-		}
-		if(contraintes.value > 3) 
-			matrice[3][2] = form.elements.x3c4.value; // c4x3
-		if(contraintes.value > 4) 
-			matrice[4][2] = form.elements.x3c5.value; // c5x3
-	}
-
-	// ************************4 variables*****************************************
-
-	if(variables.value > 3){ 
-		if(T) console.log("4 variables"); // debug
-		matrice[contraintes.value][3] = form.elements.x4max.value; // maxZ.x4
-		matrice[0][3] = form.elements.x4c1.value; // c1x4
-		matrice[1][3] = form.elements.x4c2.value; // c2x4
-		if(contraintes.value > 2) 
-			matrice[2][3] = form.elements.x4c3.value; // c3x4
-		if(contraintes.value > 3) 
-			matrice[3][3] = form.elements.x4c4.value; // c4x4
-		if(contraintes.value > 4) 
-			matrice[4][3] = form.elements.x4c5.value; // c5x4
-	}
-
-	// ************************5 variables*****************************************
-
-	if(variables.value > 4){   
-		if(T) console.log("5 variables"); // debug
-		matrice[0][4] = form.elements.x5c1.value; // c1x5
-		matrice[1][4] = form.elements.x5c2.value; // c2x5
-		matrice[contraintes.value][4] = form.elements.x5max.value; // maxZ.x5 
-		if(contraintes.value > 2) 
-			matrice[2][4] = form.elements.x5c3.value; // c3x5
-		if(contraintes.value > 3) 
-			matrice[3][4] = form.elements.x5c4.value; // c4x5
-		if(contraintes.value > 4) 
-			matrice[4][4] = form.elements.x5c5.value; // c5x5
-	}
+var matrice = new Array(); 
+var textrep;
 	
-	// *****************************determiner le pivot*************
-	
-//	if(T) console.log("init : "+ matrice[0][0]); // debug
 
-	
-	var text = '<p><label>'+maxZ.afficher()+'</p></label>';
-	document.getElementById("reponse").innerHTML = text;
-	// pour test uniquement**********************************************
-	test = false ;
-	if(test){
-	contraintes = document.getElementById("contraintes");
-	variables = document.getElementById("variables");
-	matrice[contraintes.value][0] = 4; 
-	matrice[contraintes.value][1] = 12; 
-	matrice[contraintes.value][2] = 3;
-	matrice[0][0] = 1;
-	matrice[0][1] = 0;
-	matrice[0][2] = 0; 
-	matrice[0][somme] = 1000;
-	matrice[1][0] = 0;
-	matrice[1][1] = 1;
-	matrice[1][2] = 0; 
-	matrice[1][somme] = 500;
-	matrice[2][0] = 0;
-	matrice[2][1] = 0;
-	matrice[2][2] = 1;
-	matrice[2][somme] = 1500; 
-	matrice[3][0] = 3;
-	matrice[3][1] = 6; 
-	matrice[3][2] = 2; 
-	matrice[3][somme] = 6750; 
-	}
-	}
-	// *****************************************************************
-	if(T){
-//		console.table(matrice); // debug
-	} 
-	return matrice;
-	
-//	if(T) console.table(matrice); // debug
-
-	// var text = '<p><label>x1 = </label><label id="x1"></label></br><label>x2 = </label><label id="x2"></label></br><label>x3 = </label><label id="x3"></label></br><label>x4 = </label><label id="x4"></label></br><label>x5 = </label><label id="x5"></label></br></br><label>Ooptimum est de : </label><label id="optimum"></label></p>';	
-	// algo
-};
-
-function calcul(){
-	
-	var matrice = init();	
-	var encore = true;
-	while(encore){
-		matrice = pivot(matrice);
-		if(T){
-			console.table(matrice); // debug
-		}
-	encore = false;
-	for(var i=0; i<somme; i++)
-		if(matrice[contraintes.value][i] > 0){
-			encore = true;
-		}
-		 
-	}
-	for(var i=0; i<contraintes.value; i++)
-		console.log("X"+reponse[i]+" : "+matrice[i][somme]);
-}
-
-function pivot(matrice){
-	
-//	if(T) console.log("pivot : "+ matrice[0][0]); // debug
-	// trouver le pivo
-	var max = matrice[contraintes.value][0];
-	var imax = 0;
-	for(var i=1; i<somme; i++)
-		if(matrice[contraintes.value][i] > max){
-			max = matrice[contraintes.value][i];
-			imax = i;
-		}
-	var min;
-	var imin;
-	var i=0;
-	while(lignepivot[i]){
-		i++;
-//		console.log(i);
-	}
-	min = matrice[i][somme];
-	imin = i;
-	for(var i=0; i<contraintes.value; i++)
-		if(matrice[i][somme] < min && lignepivot[i] == false){
-			min = matrice[i][somme];
-			imin = i;
-		}
-	var pivot = matrice[imin][imax];
-	lignepivot[imin] = true;
-	var rep = Number(imax)+1;
-	reponse[imin]= rep;		
-	for(var i=0; i<contraintes.value; i++){
-		console.log(i+" "+lignepivot[i]);
-	}
-	if(T) console.log(" imin : "+ imin + " imax : "+ imax + " pivot : "+ pivot); // debug
-	for(var i=0; i<=somme; i++)
-		matrice[imin][i] = matrice[imin][i]/pivot;
-	console.log("variable x"+rep+" vaut :"+matrice[imin][somme]);
-		
-	for(var i=0; i<= contraintes.value; i++){
-		if(matrice[i][imax]!=0 && i!=imin){
-			var multi = matrice[i][imax];
-			for(var j=0; j<=somme; j++){
-//			console.log(i+" "+j+" "+matrice[i][j]);
-//			console.log(i+" "+j+" "+matrice[imin][j]);
-//			console.log(i+" "+j+" "+multi*matrice[imin][j]);
-				matrice[i][j]=matrice[i][j]-multi*matrice[imin][j];
-			console.log(i+" "+j+" "+matrice[i][j]);
-			}
-		}		
-	}
-	for(var i=0; i<somme;i++)	
-//		console.log(i+" "+matrice[contraintes.value][i]);
-	return matrice;
-	}
-
-//*******************************au click****************************************
+//*******************************au click****************************************************************************
 	 function clic(){ 
 	 contraintes = document.getElementById("contraintes");
 	 variables = document.getElementById("variables");
 	 somme = Number(variables.value)+Number(contraintes.value);
 	 if(T) console.log("contraintes : "+contraintes.value+" "+"variables : "+variables.value+" somme : "+somme); //debug
-	 // construction du tableau HTML
+
+
+	 // ***********************construction du tableau HTML*****
 	 // ligne 1 
-	 var text = '<table id="tableau"><tr><td></td><th>x<sub>1</sub></th><th>x<sub>2</sub></th>';
+	 text = '<table id="tableau"><tr><td></td><th>x<sub>1</sub></th><th>x<sub>2</sub></th>';
 	 if(variables.value > 2){	
 		text += '<th>x<sub>3</sub></th>';
 		if(variables.value > 3){
@@ -392,7 +117,277 @@ function pivot(matrice){
         }
 	text += '</table></br>';
 	document.getElementById("injecter").innerHTML = text;
-	var text = '<p><label>'+maxZ.afficher()+'</p></label>';
-	document.getElementById("reponse").innerHTML = text;
+	init();
 }
+
+
+
+
+//**************************************initialisation et affichage dynamique*************************
+function init(){
+	//active le bouton calculer
+	boutonCalculer.addEventListener("click", calcul);
+	//initialise les tableaux 
+	for(var i=0; i<contraintes.value; i++){
+		lignepivot[i] = false;
+		reponse[i] = 0;
+	}
+	for(var i=0; i<Number(contraintes.value)+1;i++){
+		matrice[i] = new Array();
+		for(var j=0; j<=somme; j++)
+			matrice[i][j] = 0;	
+	}
+	// **********remplissage de la matrice**************************
+
+	// fonction max
+	matrice[contraintes.value][0] = form.elements.x1max.value; // maxZ.x1
+	matrice[contraintes.value][1] = form.elements.x2max.value; // maxZ.x2
+	// maxZ.x2 = form.elements.x2max.value;
+
+	// 2 contraintes et 2 variables  au minimum
+	matrice[0][0] = form.elements.x1c1.value; // c1x1
+	matrice[0][1] = form.elements.x2c1.value; // c1x2
+	matrice[0][somme] = form.elements.x6c1.value; // maxcontraintec1
+	matrice[1][0] = form.elements.x1c2.value; // c2x1
+	matrice[1][1] = form.elements.x2c2.value; // c2x2
+	matrice[1][somme] = form.elements.x6c2.value; // maxcontraintec2
+
+
+	// variables d'écart
+	for (var i=0; i<contraintes.value; i++)
+		matrice[i][Number(variables.value)+i] = 1;
+
+	// *****************2 variables******************************************* 
+	if(T) console.log("2 variables"); // debug
+
+	// 3 contraintes
+	if(contraintes.value > 2){
+		matrice[2][0] = form.elements.x1c3.value; // c3x1
+		matrice[2][1] = form.elements.x2c3.value; // c3x2
+		matrice[2][somme] = form.elements.x6c3.value; // maxcontraintec3
+	}
+	// 4 contraintes	
+	if(contraintes.value > 3){ 
+		matrice[3][0] = form.elements.x1c4.value; // c4x1
+		matrice[3][1] = form.elements.x2c4.value; // c4x2
+		matrice[3][somme] = form.elements.x6c4.value; // maxcontraintec4
+	}
+	// 5 contraintes	
+	if(contraintes.value > 4){ 
+		matrice[4][0] = form.elements.x1c5.value; // c5x1
+		matrice[4][1] = form.elements.x2c5.value; // c5x2
+		matrice[4][somme] = form.elements.x6c5.value; // maxcontraintec5
+	}
+
+	// ************************3 variables*****************************************
+
+	if(variables.value > 2){  
+		if(T) console.log("3 variables"); // debug
+		matrice[contraintes.value][2] = form.elements.x3max.value; // maxZ.x3
+		matrice[0][2] = form.elements.x3c1.value; // c1x3
+		matrice[1][2] = form.elements.x3c2.value; // c2x3
+		if(contraintes.value > 2){ 
+			matrice[2][2] = form.elements.x3c3.value; // c3x3
+		}
+		if(contraintes.value > 3) 
+			matrice[3][2] = form.elements.x3c4.value; // c4x3
+		if(contraintes.value > 4) 
+			matrice[4][2] = form.elements.x3c5.value; // c5x3
+	}
+
+	// ************************4 variables*****************************************
+
+	if(variables.value > 3){ 
+		if(T) console.log("4 variables"); // debug
+		matrice[contraintes.value][3] = form.elements.x4max.value; // maxZ.x4
+		matrice[0][3] = form.elements.x4c1.value; // c1x4
+		matrice[1][3] = form.elements.x4c2.value; // c2x4
+		if(contraintes.value > 2) 
+			matrice[2][3] = form.elements.x4c3.value; // c3x4
+		if(contraintes.value > 3) 
+			matrice[3][3] = form.elements.x4c4.value; // c4x4
+		if(contraintes.value > 4) 
+			matrice[4][3] = form.elements.x4c5.value; // c5x4
+	}
+
+	// ************************5 variables*****************************************
+
+	if(variables.value > 4){   
+		if(T) console.log("5 variables"); // debug
+		matrice[0][4] = form.elements.x5c1.value; // c1x5
+		matrice[1][4] = form.elements.x5c2.value; // c2x5
+		matrice[contraintes.value][4] = form.elements.x5max.value; // maxZ.x5 
+		if(contraintes.value > 2) 
+			matrice[2][4] = form.elements.x5c3.value; // c3x5
+		if(contraintes.value > 3) 
+			matrice[3][4] = form.elements.x5c4.value; // c4x5
+		if(contraintes.value > 4) 
+			matrice[4][4] = form.elements.x5c5.value; // c5x5
+
+
+	}
+
+
+	
+	// pour test uniquement**********************************************
+	test = false ;
+	if(test){
+	contraintes = document.getElementById("contraintes");
+	variables = document.getElementById("variables");
+	matrice[contraintes.value][0] = 4; 
+	matrice[contraintes.value][1] = 12; 
+	matrice[contraintes.value][2] = 3;
+	matrice[0][0] = 1;
+	matrice[0][1] = 0;
+	matrice[0][2] = 0; 
+	matrice[0][somme] = 1000;
+	matrice[1][0] = 0;
+	matrice[1][1] = 1;
+	matrice[1][2] = 0; 
+	matrice[1][somme] = 500;
+	matrice[2][0] = 0;
+	matrice[2][1] = 0;
+	matrice[2][2] = 1;
+	matrice[2][somme] = 1500; 
+	matrice[3][0] = 3;
+	matrice[3][1] = 6; 
+	matrice[3][2] = 2; 
+	matrice[3][somme] = 6750; 
+	}
+	
+	// *****************************************************************
+	if(T){
+//		console.table(matrice); // debug
+	} 
+	textrep = '<p><label>maxZ = ';
+	var plus = false;
+		for(var i=0; i<variables.value; i++){
+			if(matrice[contraintes.value][i] > 0){
+				var tampon = Number(i)+1;
+				if(plus)
+					textrep += " + ";
+                        	textrep += ""+matrice[contraintes.value][i]+" X"+tampon;
+				plus = true;
+			}
+		}
+	textrep += '</p></label></ br>';
+	textrep += '<p><label>c1 : ';
+	affiche(0);
+	textrep += '</p></label></ br>';
+	textrep += '<p><label>c2 : ';
+	affiche(1);
+	textrep += '</p></label></ br>';
+	if(contraintes.value > 2){
+		textrep += '<p><label>c3 : ';
+		affiche(2);
+		textrep += '</p></label></ br>';
+	}
+	if(contraintes.value > 3){
+		textrep += '<p><label>c4 : ';
+		affiche(3);
+		textrep += '</p></label></ br>';
+	}
+	if(contraintes.value > 4){
+		textrep += '<p><label>c5 : ';
+		affiche(4);
+		textrep += '</p></label>';
+	}
+	function affiche(num){
+	var plus = false;
+		for(var i=0; i<variables.value; i++){
+			if(matrice[num][i] > 0){
+				var tampon = Number(i)+1;
+				if(plus)
+					textrep += " + ";
+                        	textrep += ""+matrice[num][i]+" X"+tampon;
+				plus = true;
+			}
+		}
+        textrep +=" <= "+matrice[num][somme];
+	}	
+	document.getElementById("reponse").innerHTML = textrep;
+	
+	// var text = '<p><label>x1 = </label><label id="x1"></label></br><label>x2 = </label><label id="x2"></label></br><label>x3 = </label><label id="x3"></label></br><label>x4 = </label><label id="x4"></label></br><label>x5 = </label><label id="x5"></label></br></br><label>Ooptimum est de : </label><label id="optimum"></label></p>';	
+	// algo
+};
+
+function calcul(){
+	var encore = true;
+	while(encore){
+		pivot()
+		if(T){
+			console.table(matrice); // debug
+		}
+	encore = false;
+	for(var i=0; i<somme; i++)
+		if(matrice[contraintes.value][i] > 0){
+			encore = true;
+		}		 
+	}
+	for(var i=0; i<contraintes.value; i++)
+		console.log("X"+reponse[i]+" : "+matrice[i][somme]);
+}
+
+function pivot(){
+	var max = Number(matrice[contraintes.value][0]);
+	var imax = 0;
+	
+	for(var i=0; i<Number(variables.value); i++){
+		console.log("max : "+max+ "[cv][i]"+matrice[contraintes.value][i]); 
+		if(matrice[contraintes.value][i] > max){
+			max = matrice[contraintes.value][i];
+			imax = i;
+		}
+	}
+	var min;
+	var imin;
+	var i=0;
+	while(lignepivot[i]){
+		i++;
+//		console.log(i);
+	}
+	min = Number(matrice[i][somme]);
+	imin = i;
+	for(var i=0; i<contraintes.value; i++)
+		if(matrice[i][somme] < min && lignepivot[i] == false){
+			min = matrice[i][somme];
+			imin = i;
+		}
+	var pivot = matrice[imin][imax];
+	lignepivot[imin] = true;
+	var rep = Number(imax)+1;
+	reponse[imin]= rep;		
+	for(var i=0; i<contraintes.value; i++){
+//		console.log(i+" "+lignepivot[i]);
+	}
+	if(T) console.log(" imin : "+ imin + " imax : "+ imax + " pivot : "+ pivot); // debug
+	for(var i=0; i<=somme; i++)
+		matrice[imin][i] = matrice[imin][i]/pivot;
+	console.log("variable x"+rep+" vaut :"+matrice[imin][somme]);
+		
+	for(var i=0; i<= contraintes.value; i++){
+		if(matrice[i][imax]!=0 && i!=imin){
+			var multi = matrice[i][imax];
+			for(var j=0; j<=somme; j++){
+//			console.log(i+" "+j+" "+matrice[i][j]);
+//			console.log(i+" "+j+" "+matrice[imin][j]);
+//			console.log(i+" "+j+" "+multi*matrice[imin][j]);
+				matrice[i][j]=matrice[i][j]-multi*matrice[imin][j];
+//			console.log(i+" "+j+" "+matrice[i][j]);
+			}
+		}		
+	}
+	}
+
+// objects prototype
+var c1 = {
+        x1 :0, x2 :0, x3 :0, x4 :0, x5 :0, x6 :0, ecart :0,
+	afficher: function(){
+	return this.x1+" X1 + "+this.x2+" X2 + "+this.x3+" X3 + "+this.x4+" X4 + "+this.x5+" X5 <= "+this.x6;
+	}
+};
+
+var c2 = Object.create(c1);
+
+
 
